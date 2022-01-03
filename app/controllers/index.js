a63169af3cb3c93c867b1ff1d51106e66def453a
Fabricio Araujo <fabricio.info2@gmail.com>
2022-01-03 00:36:57 -0300

    auth controller

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,3 +1,19 @@
 module.exports.index = function(application, req, res) {
-    res.render('index')
+    res.render('index', {validation: {}})
+}
+
+module.exports.autenticar = function(application, req, res) {
+    var dadosForm = req.body;
+
+    req.assert('usuario', 'User cannot be empty').notEmpty();
+    req.assert('senha', 'Password cannot be empty').notEmpty();
+
+    var errors = req.validationErrors();
+
+    if(errors) {
+        res.render('index', {validation: errors});
+        return;
+    }
+
+    res.send('Tudo ok para criar a sessão');
 }
