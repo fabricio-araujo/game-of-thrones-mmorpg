a48105772abe290da8a47742cc5265b1c2f5f224
Fabricio Araujo <fabricio.info2@gmail.com>
2022-02-05 10:29:41 -0300

    crud actions

--- app/models/JogoDAO.js
+++ app/models/JogoDAO.js
@@ -1,3 +1,7 @@
+var ObjectId = require('mongodb').ObjectId;
+
+const res = require("express/lib/response");
+
 function JogoDAO(connection) {
     this._connection = connection();
 }
@@ -19,15 +23,87 @@ JogoDAO.prototype.gerarParametros = function(usuario) {
     });
 }
 
-JogoDAO.prototype.iniciaJogo = function(res, usuario, casa, comando_invalido) {
+JogoDAO.prototype.iniciaJogo = function(res, usuario, casa, msg) {
     this._connection.open(function(err, mongoClient) {
         mongoClient.collection('jogo', function(err, collection) {
             collection.find({usuario: usuario}).toArray(function(err, result){
                 
-	            res.render("jogo", {img_casa: casa, jogo: result[0], comando_invalido: comando_invalido});
+	            res.render("jogo", {img_casa: casa, jogo: result[0], msg: msg});
                 
                 mongoClient.close();
             });
+        });
+    });
+}
+
+JogoDAO.prototype.acao = function(acao) {
+    this._connection.open(function(err, mongoClient) {
+        mongoClient.collection('acao', function(err, collection) {
+            
+            var date = new Date();
+
+            var tempo = null;
+
+            switch(parseInt(acao.acao)) {
+                case 1: tempo = 1 * 60 * 60000; break;
+                case 2: tempo = 2 * 60 * 60000; break;
+                case 3: tempo = 5 * 60 * 60000; break;
+                case 4: tempo = 5 * 60 * 60000; break;
+            }
+            
+            acao.acao_termina_em = date.getTime() + tempo;
+
+            collection.insert(acao);
+            
+        });
+        
+        mongoClient.collection('jogo', function(err, collection) {
+
+            var moedas = null;
+            
+            switch(parseInt(acao.acao)) {
+                case 1: moedas = -2 * acao.quantidade; break;
+                case 2: moedas = -3 * acao.quantidade; break;
+                case 3: moedas = -1 * acao.quantidade; break;
+                case 4: moedas = -1 * acao.quantidade; break;
+            }
+            collection.update(
+                { usuario: acao.usuario },
+                { $inc: {moeda: moedas} }
+            );
+
+            mongoClient.close();
+        });
+    });
+}
+
+JogoDAO.prototype.getAcoes = function(usuario, res) {
+    this._connection.open(function(err, mongoClient) {
+        mongoClient.collection('acao', function(err, collection) {
+
+            var date = new Date();
+            var momento_atual = date.getTime();
+
+            collection.find({usuario: usuario, acao_termina_em: {$gt:momento_atual}}).toArray(function(err, result){
+               
+                res.render("pergaminhos", {acoes: result});
+	           
+                mongoClient.close();
+            });
+        });
+    });
+}
+
+JogoDAO.prototype.revogarAcao = function(_id, res) {
+    this._connection.open(function(err, mongoClient) {
+        mongoClient.collection('acao', function(err, collection) {
+            collection.remove(
+                {_id : ObjectId(_id)},
+                function(err, result) {
+                    res.redirect('jogo?msg=D');
+                    mongoClient.close();
+                }
+            );
         });
     });
 }
